e645839820cca42ddc34fd5b94379558e7cbf785
JuniorGassam <117817188+JuniorGassam@users.noreply.github.com>
2023-10-13 23:48:58 +0200

    Update actualite.php

--- actualite.php
+++ actualite.php
@@ -18,7 +18,7 @@ if(isset($_GET['id']) AND !empty($_GET['id'])){
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-        <img src="./uploads/articles/3-devops.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        <img src="./uploads/articles/<?= $article['image'] ?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
         <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?= $article['title'] ?></h1>
